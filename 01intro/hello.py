bf2ed468596d50a46b67972c6d9eec54d4a8a148
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2024-06-27 21:29:30 +0300

    intro

---
+++ 01intro/hello.py
@@ -0,0 +1,69 @@
+# print("Hello")
+# print(1)
+# print(type(1))
+# print(1.4)
+# print(type(1.4))
+# print(1j)
+# print(type(1j))
+# print(1, type(1))
+# print(1.4, type(1.4))
+# print(1j, type(1j))
+# print(float(1), type(1), type(float(1)))
+# print(int(4.5), type(4.5), type(int(4.5)))
+# print(5+2)
+# print(4-2)
+# print(4*3)
+# print(5/3)
+# print(7 % 4)
+# print(7//4)
+# print(4**3)
+# print(1 == 5)
+# print(5 == 5)
+# print(1 != 5)
+# print(5 > 3)
+# print(5 < 3)
+# print(5 <= 3)
+# print(5 <= 5)
+# print(7 >= 7)
+# print(3 <= 5)
+# AND
+# print(4 < 5 and 4 < 3 and 7 > 4)
+# true and false => false
+# false and true => false
+# true and true => true
+# false and false => false
+# OR
+# print(4 < 5 or 4 < 3 or 7 > 4)
+# true or false => true
+# false or true => true
+# true or true => true
+# false or false => false
+# print(not(4 < 5 and 4 > 2))
+# print(not(5 > 4 > 2))
+# print(not(4 > 5 and 3 > 2))
+# print(7 is 7)
+# print(7 is not 7)
+# print(7 is None)
+# print(1 in [1, 2, 3])
+# print('mere' in ['pere', 'mere', 'suc', 'alune'])
+# print('mere' not in ['pere', 'mere', 'suc', 'alune'])
+# print('pan' in 'paine')
+# print('r' in 'paine')
+# print('1' in 'paine1')
+# print(str(1) in 'paine1')
+# variabila_noua1 = None
+# variabila_noua1 = '1.56'
+# variabila_noua2 = 1.56
+# print(type(variabila_noua1))
+# word = 'Ana are mere'
+# word = "Ana are mere"
+numar_mere = 3
+numar_pere = 4
+# word = "Ana are " + str(numar_mere) + " mere si " + str(numar_pere)
+# word = f"Ana are '{numar_mere}' mere si {numar_pere} pere"
+# word = f'Ana are "{numar_mere}" mere si {numar_pere} pere'
+# word = f'Ana are \'{numar_mere}\' mere si {numar_pere} pere'
+# word = f"Ana are \"{numar_mere}\" mere si {numar_pere} pere"
+# word = "Ana are {} mere si {} pere".format(numar_mere, numar_pere)
+word = "\tAna are {1} mere \nsi {0} pere".format(numar_mere, numar_pere)
+print(word)
